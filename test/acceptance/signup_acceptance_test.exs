6b345636fd10448f75d6d2a0add0ce5c3533be2d
Aaron Tinio <aptinio@gmail.com>
2016-04-08 09:37:54 +0800

    Use a comparison operator for the assertion expression
    to show the values of the two sides

--- test/acceptance/signup_acceptance_test.exs
+++ test/acceptance/signup_acceptance_test.exs
@@ -78,11 +78,11 @@ defmodule AppPrototype.SignupAcceptanceTest do
     navigate_to "/"
 
     GetStartedForm.submit(email: "")
-    assert String.contains?(visible_page_text, "can't be blank")
+    assert visible_page_text =~ "can't be blank"
 
     GetStartedForm.submit(email: "foo@bar.baz")
-    assert String.contains?(visible_page_text, "Thanks")
-    assert String.contains?(visible_page_text, "We sent you an email at foo@bar.baz")
+    assert visible_page_text =~ "Thanks"
+    assert visible_page_text =~ "We sent you an email at foo@bar.baz"
 
     email = Email |> last(:inserted_at) |> Repo.one
     assert "foo@bar.baz" == email.address
@@ -93,23 +93,23 @@ defmodule AppPrototype.SignupAcceptanceTest do
                       last_name: "",
                       org: "",
                       password: "")
-    assert String.contains?(visible_page_text, "Organization\ncan't be blank")
-    assert String.contains?(visible_page_text, "First name\ncan't be blank")
-    assert String.contains?(visible_page_text, "Password\ncan't be blank")
+    assert visible_page_text =~ "Organization\ncan't be blank"
+    assert visible_page_text =~ "First name\ncan't be blank"
+    assert visible_page_text =~ "Password\ncan't be blank"
 
     SignupForm.submit(first_name: "Foo",
                       last_name: "Bar",
                       org: "Bar Corp.",
                       password: "foobarbaz")
-    assert String.contains?(visible_page_text, "Registration successfully completed.")
+    assert visible_page_text =~ "Registration successfully completed."
 
     LoginForm.log_in(email: "foo@bar.baz", password: "wrong")
-    assert String.contains?(visible_page_text, "Incorrect email or password.")
+    assert visible_page_text =~ "Incorrect email or password."
 
     LoginForm.log_in(email: "wrong@bar.baz", password: "foobarbaz")
-    assert String.contains?(visible_page_text, "Incorrect email or password.")
+    assert visible_page_text =~ "Incorrect email or password."
 
     LoginForm.log_in(email: "foo@bar.baz", password: "foobarbaz")
-    assert String.contains?(visible_page_text, "Logged in as Foo.")
+    assert visible_page_text =~ "Logged in as Foo."
   end
 end
