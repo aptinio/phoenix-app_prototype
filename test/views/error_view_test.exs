76135ba8ab4296a5e75a6096152487ace643f0cd
Aaron Tinio <aptinio@gmail.com>
2016-04-11 16:36:43 +0800

    Add a template for errors

--- test/views/error_view_test.exs
+++ test/views/error_view_test.exs
@@ -5,17 +5,17 @@ defmodule AppPrototype.ErrorViewTest do
   import Phoenix.View
 
   test "renders 404.html" do
-    assert render_to_string(AppPrototype.ErrorView, "404.html", []) ==
+    assert render_to_string(AppPrototype.ErrorView, "404.html", []) =~
            "Page not found"
   end
 
   test "render 500.html" do
-    assert render_to_string(AppPrototype.ErrorView, "500.html", []) ==
+    assert render_to_string(AppPrototype.ErrorView, "500.html", []) =~
            "Server internal error"
   end
 
   test "render any other" do
-    assert render_to_string(AppPrototype.ErrorView, "505.html", []) ==
+    assert render_to_string(AppPrototype.ErrorView, "505.html", []) =~
            "Server internal error"
   end
 end
--- web/views/error_view.ex
+++ web/views/error_view.ex
@@ -2,11 +2,11 @@ defmodule AppPrototype.ErrorView do
   use AppPrototype.Web, :view
 
   def render("404.html", _assigns) do
-    "Page not found"
+    render "error.html", %{reason: "Page not found"}
   end
 
   def render("500.html", _assigns) do
-    "Server internal error"
+    render "error.html", %{reason: "Server internal error"}
   end
 
   # In case no render clause matches or no
